3085a3ac39248c4464a5455b62443187b5b4520f
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-30 21:37:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,12 +10,15 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute_category, attribute_value|
     attribute_value.each do |attribute, pigeon_name|
       pigeon_name.each do |name|
+        
        if pigeon_list[name] == nil # Checks to see if name is already a key
         pigeon_list[name] = {}
        end
        
        if pigeon_list[name][attribute_category] == nil
-         
+         pigeon_list[name][attribute_category] = {}
+       end 
+       
       end
     end
   end
