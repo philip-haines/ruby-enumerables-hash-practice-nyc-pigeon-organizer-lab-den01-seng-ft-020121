c44c2366e3e9380639123f60ba459d0fa7bcbac4
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-30 21:42:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,17 +7,22 @@ def nyc_pigeon_organizer(data)
   
   pigeon_list = {}
   
-  data.each do |attribute_category, attribute_value|
+  data.each do |attribute_category, attribute_value| 
+  # Cateogry: Color, Gender, Lives
+  # Value: (purple, grey, white, brown) (male, female) (subway, Central Park, Library)
+  
     attribute_value.each do |attribute, pigeon_name|
+      
       pigeon_name.each do |name|
         
        if pigeon_list[name] == nil # Checks to see if name is already a key
         pigeon_list[name] = {}
        end
        
-       if pigeon_list[name][attribute_category] == nil
-         pigeon_list[name][attribute_category] = {}
+       if pigeon_list[name][attribute_category] == nil # Checks for key "atribute category" key
+         pigeon_list[name][attribute_category] = []
        end 
+       
        
       end
     end
